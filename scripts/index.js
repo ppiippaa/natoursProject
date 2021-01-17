49b8cfd6feeb635c8e6f2c678da871f4b445788d
Pippa <pip.thompson96@gmail.com>
2021-01-17 13:28:04 +0000

    add media queries to pop-up

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,13 @@ $(".navigation__link").click( function() {
    $(".navigation__button").trigger("click");
 })
 
-$(document).on('click', 'a[href^="#"]', function (event) {
+$('.smooth-scroll').click(function (event) {
     event.preventDefault();
 
     $('html, body').delay(500).animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 800);
 });
+
+
+
